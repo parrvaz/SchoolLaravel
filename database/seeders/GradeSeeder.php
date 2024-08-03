de0fcd3bc48f9acd75e0c5d50ffe39ed1c9c54f1
parrvaz <47025465+parrvaz@users.noreply.github.com>
2024-08-03 15:37:30 +0330

    migration and seed comp

--- database/seeders/GradeSeeder.php
+++ database/seeders/GradeSeeder.php
@@ -16,14 +16,15 @@ class GradeSeeder extends Seeder
         DB::table('grades')->insert([[
             'id' => 10,
             'number' => 10,
-        ],[
+        ],
+        [
             'id' => 11,
             'number' => 11,
         ],
-            [
+        [
                 'id' => 12,
                 'number' => 12,
-            ]
+        ]
         ]);
     }
 }

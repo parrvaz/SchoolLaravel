dd55952638b4b88d0f72efa7c2ee4e64d9c354b3
parrvaz <47025465+parrvaz@users.noreply.github.com>
2024-09-20 14:04:49 +0330

    messageBox start

---
+++ app/Http/Controllers/MessageBoxController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MessageBoxController extends Controller
+{
+    public function sendMessage(Request $request,MessageBoxValidation $validation){
+
+    }
+}
---
+++ app/Http/Requests/MessageBox/MessageBoxValidation.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests\MessageBox;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class MessageBoxValidation extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+    public function rules(): array
+    {
+        return [
+
+        ];
+    }
+}
